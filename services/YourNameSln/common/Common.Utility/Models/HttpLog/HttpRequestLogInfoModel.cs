b2217a6d055b2e06e416201be8081a24afefc30c
caijinhao-home <531003539@qq.com>
2020-05-16 15:27:16 +0800

    添加httpLog

---
+++ services/YourNameSln/common/Common.Utility/Models/HttpLog/HttpRequestLogInfoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Utility.Models.Log
+{
+    /// <summary>
+    /// INFO 日志
+    /// </summary>
+    public class HttpRequestLogInfoModel
+    {
+        public string Url { get; set; }
+        public string QueryData { get; set; }
+        public string BodyData { get; set; }
+        public string Ip { get; set; }
+        public LogInfoType LogType { get; set; }
+        public string LogTypeMsg { get; set; }
+        public string Method { get; set; }
+        public string ContentType { get; set; }
+        public List<string> Headers { get; set; }
+        public string ResponseBody { get; set; }
+        public HttpRequestLogInfoModel(LogInfoType requestLogType)
+        {
+            LogType = requestLogType;
+            SetRequestLogTypeMsg();
+        }
+
+        private void SetRequestLogTypeMsg()
+        {
+            switch (LogType)
+            {
+                case LogInfoType.API:
+                    LogTypeMsg = "API";
+                    break;
+                case LogInfoType.Other:
+                    LogTypeMsg = "其他";
+                    break;
+                default:
+                    break;
+            }
+            LogTypeMsg += "请求";
+        }
+    }
+
+    public enum LogInfoType
+    { 
+        API=1,
+        Other=2
+    }
+}
